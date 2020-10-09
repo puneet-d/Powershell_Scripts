f83bbe5cf59156cca9507f3df0d0e585c445e030
Puneet Dixit <puneet-d@users.noreply.github.com>
2020-10-10 05:07:15 +0530

    Update aduser_object_change.ps1

--- aduser_object_change.ps1
+++ aduser_object_change.ps1
@@ -1,7 +1,10 @@
-$user = Get-Content .\Desktop\user.txt
+
+Import-Module ActiveDirectory
+
+$user = Get-Content .\user.txt
 foreach ($line in $user){
 Get-ADUser -Identity $line |Get-ADObject |Set-ADObject -ProtectedFromAccidentalDeletion $True
-Get-ADUser -Identity $line -properties * | Export-Csv .\Desktop\test.csv
+Get-ADUser -Identity $line -properties * | Export-Csv .\test.csv
 }
 
 
