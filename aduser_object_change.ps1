a5b3849aae213f73ae03af1c246a0eba7bc6916d
Puneet Dixit <puneet-d@users.noreply.github.com>
2020-10-10 06:02:41 +0530

    Update aduser_object_change.ps1

--- aduser_object_change.ps1
+++ aduser_object_change.ps1
@@ -1,10 +1,12 @@
-
 Import-Module ActiveDirectory
 
 $user = Get-Content .\user.txt
 foreach ($line in $user){
-Get-ADUser -Identity $line |Get-ADObject |Set-ADObject -ProtectedFromAccidentalDeletion $True
-Get-ADUser -Identity $line -properties * | Export-Csv .\test.csv
+try{
+	Get-ADUser -Identity $line |Get-ADObject |Set-ADObject -ProtectedFromAccidentalDeletion $True
+	Get-ADUser -Identity $line -properties * | Export-Csv .\test.csv
 }
-
-
+catch{
+	Write-Host "User $line is not available."
+}
+}
